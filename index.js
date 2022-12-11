4640936ef6f7979c507d93a205f40d2e9f2a24e8
Medidanagaraju <nagarajumedida123@gmail.com>
2022-12-11 20:33:50 +0530

    intial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+require("dotenv").config()
+
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.get("/name",(req,res)=>{
+    res.send(`The name is ${process.env.NAME}`)
+})
+app.listen(9000,()=>{
+    console.log("Listeniing port 900")
+})
